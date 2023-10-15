07bb0d138173e6470150e630dbe84ba3d336903d
StephBen <stefanos.mpeniamin@gmail.com>
2023-10-15 12:25:55 +0300

    Predicted the positions when samles are following a line

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 def logNormalGen(amount):
+    """
+       Generates an array of log-normal distributed values and scales them.
+
+       Parameters:
+       amount (int): The number of log-normal samples to generate.
+
+       Returns:
+       numpy.ndarray: An array of log-normal samples, scaled by the minimum value.
+    """
     data = []
     for i in range(amount):
         sample = 10. + np.random.standard_normal(100)
@@ -13,36 +22,65 @@ def logNormalGen(amount):
 
 
 def lineDataGen(amount, a, b):
-    data=[]
-    data.append(0)
-    for i in range(amount-1):
-        data.append(a*i+b)
-    return np.array(data)
+    """
+    Generates linear data and labels.
+
+    Parameters:
+    amount (int): The number of data points to generate.
+    a (float): The slope of the linear relationship.
+    b (float): The y-intercept of the linear relationship.
+
+    Returns:
+    numpy.ndarray, numpy.ndarray: Arrays containing generated data and labels.
+    """
+    data= []
+    labels = []
+    for i in range(amount):
+        data.append(i)
+        labels.append(2*i+1)
+    return np.array(data), np.array(labels)
 
 
 def train(data, positions):
+    """
+    Trains a linear regression model and visualizes the data.
+
+    Parameters:
+    data (list or array-like): Input data.
+    positions (list or array-like): Corresponding positions or labels for the data.
+
+    Returns:
+    sklearn.linear_model.LinearRegression: Trained linear regression model.
+    """
     X = np.array(data).reshape(-1,1)
     Y = np.array(positions).reshape(-1,1)
     reg = LinearRegression()
-    reg.fit(Y,X)
-    plt.scatter(Y,X)
+    reg.fit(X,Y)
+    plt.scatter(X,Y)
+    plt.title("Data")
     plt.show()
     return reg
 
+
+def modelPrecision(predictions, labels):
+    score = 0
+    for i in range(100):
+        if predictions[i][0] == labels[i][0]:
+            score = score + 1
+    return score
+
 def main():
 
-    linear = lineDataGen(100, 2, 1)
-    labels = np.arange(len(linear), dtype=np.int32)
-    linearDf = pd.DataFrame(linear)
-    model = train(linear, labels)
-    predictions = model.predict(linear.reshape(-1,1))
-    # predictions = predictionRound(predictions)
-    print(linear)
-    print(np.rint(predictions))
-    print(len(predictions))
-    print(len(linear))
-    plt.scatter(labels.reshape(-1,1), linear.reshape(-1,1))
-    plt.scatter(labels.reshape(-1,1), predictions, color='red')
+    keys,labels = lineDataGen(100, 2, 1) #Generate 100 samples on the 2x + 1 line
+    model = train(keys, labels) # Train a linear Regression Model on the generated keys and labels
+    predictions = model.predict(keys.reshape(-1,1)) # Predict the position of the keys generated with the model.
+    print(keys)
+    print(labels)
+    print(predictions)
+    score = modelPrecision(np.rint(predictions), labels.reshape(-1,1)) # Calculate precision after rounding the samples
+    print(str(score) + ' out of ' + str(len(keys)) + ' rounded samples predicted right')
+    plt.scatter(keys.reshape(-1,1), predictions, color='red') # Plot the predictions.
+    plt.title("Predictions")
     plt.show()
 
 if __name__ == '__main__':
